f683bebaba5b5566fdd5ebe19429d55f60ae670c
Kenia Guimaraes <keniiarruda@gmail.com>
2019-06-26 21:13:02 +0000

    Retira requisição de biblioteca.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require activestorage
 //= require turbolinks
 //= require jquery
-//= require jquery-ujs
 //= require materialize-sprockets
 //= require select2
 //= require materialize/extras/nouislider
