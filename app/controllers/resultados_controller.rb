1629da6b1900f1bb6c2068a261dca4b1861ee659
Kenia Guimaraes <keniiarruda@gmail.com>
2019-07-09 19:11:49 +0000

    Adiciona mais ajustes no controler de resultados.

--- app/controllers/resultados_controller.rb
+++ app/controllers/resultados_controller.rb
@@ -31,52 +31,8 @@ class ResultadosController < ApplicationController
                                     .joins("JOIN semestres ON semestres.id = avaliacaoprofs.semestre_id" )
                                     .group("cast(semestres.ano as char(4))||cast(semestres.codigo as char(1))").all 
     end 
-    
-    def comentario
-    
 
-    end
-    
-    
-    def apurarcoment
 
-          @qtd_comentarios = Comentar.all   
-                                    
-          @qtd_disciplinas = Comentar.select("disciplinacursos.disciplina_id")
-                                    .joins("JOIN disciplinacursos ON disciplinacursos.id = comentars.disciplinacurso_id" )
-                                    .group("disciplinacursos.disciplina_id").all                                  
-        
-          
-          @qtd_usuarios    = Comentar.select("user_id as qtd_usuario").group(:user_id).all
-          
-          @resultados      = Resultado.where("tiporesultado_id = 1").all
- 
-
-          if  @resultados.empty?
-            @apura_comentario = Resultado.new()
-            @apura_comentario.tiporesultado_id  = 1
-            @apura_comentario.data_apuracao  =  DateTime.now 
-            @apura_comentario.data_abertura  =  nil
-            @apura_comentario.semestre_id    =  3
-            @apura_comentario.qtd_disciplinas          = @qtd_disciplinas.sizes
-            @apura_comentario.qtd_comentarios          = @qtd_comentarios.size
-            @apura_comentario.qtd_usuarios             = @qtd_usuarios.size
-            @apura_comentario.qtd_docentes             =  0
-            @apura_comentario.qtd_avaliacoes           = 0
-            @apura_comentario.qtd_boa_didatica         = 0
-            @apura_comentario.qtd_docentes             = 0
-            @apura_comentario.qtd_avaliacoes           = 0
-            @apura_comentario.qtd_bom_relacionamento   = 0
-            @apura_comentario.qtd_bom_dominio          = 0
-            @apura_comentario.qtd_nivel_recomendacao   = 0
-            if @apura_comentario.save
-              redirect_to "/resultados", notice: 'A apuração dos comentarios foi realizada com sucesso!'  
-            else
-              redirect_to "/resultados", notice: 'Não'  
-            end
-          else
-          end
-    end
     
     
     def gerarelcoment
