6f6f9db1b9c032dceeb8081ab7dbf0196d399bf1
Kenia Guimaraes <keniiarruda@gmail.com>
2019-07-10 02:05:24 +0000

    Adiciona melhoria na navbar.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,10 +3,10 @@ class Ability
   
   
 
-  def initialize(usuario)
+  def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-       usuario ||= Usuario.new # guest user (not logged in)
+       user ||= Usuario.new # guest user (not logged in)
     #   if user.admin?
     #     can :manage, :all
     #   else
