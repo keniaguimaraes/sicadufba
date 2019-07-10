33fb2d434cc4d72a84c81b16effcb1690c4a4161
Kenia Guimaraes <keniiarruda@gmail.com>
2019-07-10 01:58:44 +0000

    Volta configuração no root para logout.

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,7 @@ resources :admin do
 
  devise_scope :user do
     get '/login' => 'devise/sessions#new'
-    get '/logout' => 'https://autenticacao.ufba.br/ca/logout'
+    get '/logout' => 'devise/sessions#destroy'
    # get '/perfil' => 'devise/registrations#edit'
     get '/user' => 'user#index'
  end
