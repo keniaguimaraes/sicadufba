c4abf0a18f297ea64950567ebcf6cdd4e3e44dc1
Kenia Guimaraes <keniiarruda@gmail.com>
2019-07-11 21:20:25 +0000

    Adiciona permissões de usuarios.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -14,13 +14,11 @@ class Ability
  
     if user.administrador? 
       can :manage, :all
-   else
-      alias_action  :update  :to => :crud2 
-      alias_action  :create, :read, :update, :destroy,  :to => :crud 
-      can :crud, Avaliacaoprof
-      can :crud, Comentar
-      can :crud2, Usuario      
-   end
+    else
+      can :manage, Avaliacaoprof
+      can :manage, Comentar
+      can :read, Usuario      
+    end
     
      
     # The first argument to `can` is the action you are giving the user
