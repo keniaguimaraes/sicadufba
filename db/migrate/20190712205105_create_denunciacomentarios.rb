d961d45de1bada4ea3e5369577edfdd9a6d34488
Kenia Guimaraes <keniiarruda@gmail.com>
2019-07-19 20:38:27 +0000

    Adiciona correção na classe de denuncicomentário.

--- db/migrate/20190712205105_create_denunciacomentarios.rb
+++ db/migrate/20190712205105_create_denunciacomentarios.rb
@@ -2,7 +2,7 @@ class CreateDenunciacomentarios < ActiveRecord::Migration[5.2]
   def change
     create_table :denunciacomentarios do |t|
       t.integer :usuario_id
-      t.string  :comentar_id
+      t.integer  :comentar_id
       t.timestamps
     end
   end
